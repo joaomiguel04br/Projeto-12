f4f7f95be695bdb7ce2eb44dbdb8a971b6830ca3
joaomiguel04br <101592735+joaomiguel04br@users.noreply.github.com>
2022-11-07 15:50:27 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
+
+function preload(){
+ pathImg = loadImage("path.png")
+ boyImg = loadAnimation("Jake1.png","Jake2.png","jake3.png","jake4.PNG","jake5.png")
+ 
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  path = createSprite(200,200,400,50)
+  path.addImage(pathImg)
+path.scale=1.2;
+
+boy = createSprite(200,200,30,30)
+boy.addAnimation("jose",boyImg)
+boy.scale= 0.5;
+  
+
+leftBoundary=createSprite(0,0,100,800);
+leftBoundary.visible = false
+
+
+rightBoundary=createSprite(410,0,100,800);
+rightBoundary.visible = false
+}
+
+function draw() {
+  background(0);
+  path.velocityY = 4;
+  
+  boy.x = World.mouseX
+  
+  edges= createEdgeSprites();
+  boy.collide(edges[3]);
+  boy.collide(rightBoundary)
+  boy.collide(leftBoundary)
+  if(path.y > 400 ){
+    path.y = height/2;
+  }
+  
+  drawSprites();
+}
